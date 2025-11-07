28100c2f240a3035385a4fabb93b1a844474709d
andrewkozinski <andrew.kozinski@optimum.com>
2025-11-07 17:29:50 -0500

    Initial backend commit, set up cmd directory, simple listen and server function call in main for now

---
+++ backend/cmd/movie/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Println("Starting GO TMDB API service")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
