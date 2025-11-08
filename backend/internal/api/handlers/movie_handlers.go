34f4f8ddc67025a39a7abee64ea94aecfc1e1a55
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-07 20:59:18 -0500

    Set up route to get movie details

--- backend/internal/api/handlers/movie_handlers.go
+++ backend/internal/api/handlers/movie_handlers.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"backend/internal/api/model"
+	"backend/internal/api/tmdb"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -10,7 +13,7 @@ import (
 )
 
 // MovieHandler Handles getting the information for each route. So getting trending movies & movie information
-func MovieHandler(router *chi.Mux) {
+func MovieHandler(router *chi.Mux, client *tmdb.TMDBClient) {
 
 	//Following allows trailing slashes to be accepted for routes and not throw a 404
 	router.Use(chimiddle.StripSlashes)
@@ -32,10 +35,32 @@ func MovieHandler(router *chi.Mux) {
 
 	router.Route("/movie", func(r chi.Router) {
 		r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
-			_, err := fmt.Fprintf(w, "Movie API Response for id: "+chi.URLParam(r, "id"))
-			if err != nil {
+
+			id := chi.URLParam(r, "id")                   //get the movie id
+			movie, movieErr := client.GetMovieDetails(id) //fetch movie details from TMDB
+			if movieErr != nil {
+				http.Error(w, "Failed to fetch movie details", http.StatusInternalServerError)
 				return
 			}
+
+			//Once done with the response body, close it
+			defer movie.Body.Close()
+
+			var movieData model.Movie //Movie struct to hold the decoded data
+			//Actually decode the data into the struct
+			jsonErr := json.NewDecoder(movie.Body).Decode(&movieData)
+
+			if jsonErr != nil {
+				http.Error(w, "Failed to decode movie details", http.StatusInternalServerError)
+				return
+			}
+
+			//Before sending the response, properly format the PosterPath (docs for reference: https://developer.themoviedb.org/docs/image-basics)
+			movieData.PosterPath = "https://image.tmdb.org/t/p/w500" + movieData.PosterPath
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(movie.StatusCode)
+			_ = json.NewEncoder(w).Encode(movieData)
 		})
 	})
 
