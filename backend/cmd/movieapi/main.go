aadf1c5e316d66963fbd01312b68763583634c87
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-07 20:59:39 -0500

    Loaded the apiKey from the .env file and passed it to the MovieHandler

--- backend/cmd/movieapi/main.go
+++ backend/cmd/movieapi/main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"backend/internal/api/handlers"
+	"backend/internal/api/tmdb"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	//Load TMDB key from the environment file
+	loadErr := godotenv.Load()
+	if loadErr != nil {
+		log.Println("Error loading .env file")
+	}
+	apiKey := os.Getenv("TMDB_KEY")
+
 	router := chi.NewRouter()
 
 	//router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 	//	fmt.Fprintf(w, "Movie API Response")
 	//})
 
-	handlers.MovieHandler(router)
+	client := tmdb.NewTMDBClient(apiKey)
+
+	handlers.MovieHandler(router, client)
 
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
