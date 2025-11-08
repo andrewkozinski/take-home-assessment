f2d6c90aa04e01f1d8aa1614b4caa62cccf2f335
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-08 16:43:25 -0500

    Added comments for set and get cache

--- frontend/lib/cache.ts
+++ frontend/lib/cache.ts
@@ -1,4 +1,11 @@
 //Cache implementation using LocalStorage with expiration
+
+/**
+ * Sets an item in the cache with a specified TTL (time to live)
+ * @param key Key of the item to be cached
+ * @param data The actual data, ex: Movie details
+ * @param ttl The time to live
+ */
 export function setCache(key: string, data: any, ttl: number) {
     const now = new Date();
     const item = {
@@ -8,6 +15,11 @@ export function setCache(key: string, data: any, ttl: number) {
     localStorage.setItem(key, JSON.stringify(item));
 }
 
+/**
+ * Retrieves an item from the cache
+ * @param key Key of the cached item
+ * @return The cached data or null if not found/expired
+ */
 export function getCache(key: string) {
     const itemStr = localStorage.getItem(key);
     if (!itemStr) {
