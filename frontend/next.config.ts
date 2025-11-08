9183b296277dcd1d91687ab96838f163e53cc0da
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-08 12:35:01 -0500

    Added TMDB as an allowed image source in next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  //Add the TMDB url as an allowed image source
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+    ],
+  },
+
 };
 
 export default nextConfig;
