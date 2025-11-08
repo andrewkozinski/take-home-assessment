40b908154731966f6eb9d0960986f64d70963502
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-08 14:10:01 -0500

    Added error handling in case of backend being down

--- frontend/components/ui/trending-movies-list.tsx
+++ frontend/components/ui/trending-movies-list.tsx
@@ -7,13 +7,21 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 
 // This component displays a list of trending movies
 export default function TrendingMoviesList({ timeFrame }: { timeFrame: TimeFrame }) {
-    
+
     const [movies, setMovies] = useState<Movie[]>([]);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         async function fetchTrending() {
-            const data = await fetchTrendingMovies(timeFrame);
-            setMovies(data);
+            try {
+                const data = await fetchTrendingMovies(timeFrame);
+                setMovies(data);
+            }
+            catch (error) {
+                console.error("Error fetching trending movies: ", error);
+                setMovies([]);
+                setError((error as Error).message);
+            }
         }
         fetchTrending();
     }, [timeFrame]);
@@ -27,6 +35,20 @@ export default function TrendingMoviesList({ timeFrame }: { timeFrame: TimeFrame
         return result;
     }
     const movieChunks = splitMovies(movies, 5);//Splits the movies into chunks of 5
+
+    if(error) {
+        return (
+            <div className="justify-center items-center flex flex-col">
+                <p className="text-red-1000">Error loading trending movies. Please try again later.</p>
+                <p>Error Message: {error}</p>
+            </div>
+
+        )
+    }
+
+    if(movies.length === 0) {
+        return <div>No trending movies found. Please try again later.</div>;
+    }
 
     return (
         <div className="">
