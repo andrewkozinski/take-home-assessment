bd1affd454fb910ba8cba89fbd05d53dabca1f96
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-08 15:25:16 -0500

    Added favorite button to movie page

--- frontend/app/movie/[id]/page.tsx
+++ frontend/app/movie/[id]/page.tsx
@@ -6,6 +6,9 @@ import { useEffect, useState } from "react";
 import { Badge } from "@/components/ui/badge"
 import { fetchMovieDetails } from "@/lib/movies";
 import { Navbar } from "@/components/ui/navbar";
+import { Toggle } from "@/components/ui/toggle";
+import { addFavoriteMovie, removeFavoriteMovie, isMovieFavorited } from "@/lib/favorites";
+import { StarIcon } from "lucide-react";
 
 export default function MoviePage() {
     //Get the movie ID from the URL params
@@ -14,6 +17,7 @@ export default function MoviePage() {
     //Fetch movie data from nextjs api route
     const [movie, setMovie] = useState<Movie | null>(null);
     const [error, setError] = useState<string | null>(null);
+    const [isFavorited, setIsFavorited] = useState<boolean>(false);
 
     useEffect(() => {
         async function fetchMovie() {
@@ -26,6 +30,13 @@ export default function MoviePage() {
             }
         }
         fetchMovie();
+        //Check if movie is favorited
+        if (id) {
+            const favorited = isMovieFavorited(Number(id));
+            setIsFavorited(favorited);
+            console.log("Is movie favorited: ", favorited);
+            console.log(localStorage.getItem("favoriteMovies"));
+        }
     }, [id]);
 
     if (error) {
@@ -65,6 +76,24 @@ export default function MoviePage() {
                             ))
                         }
                     </div>
+                    <Toggle 
+                        aria-label="Toggle bookmark"
+                        size="sm"
+                        variant="outline"
+                        className="data-[state=on]:bg-transparent data-[state=on]:*:[svg]:fill-blue-500 data-[state=on]:*:[svg]:stroke-blue-500"
+                        pressed={isFavorited}
+                        onPressedChange={(pressed) => {
+                            if (pressed) {
+                                addFavoriteMovie(movie.id);
+                            } else {
+                                removeFavoriteMovie(movie.id);
+                            }
+                            setIsFavorited(pressed);
+                        }}
+                    >
+                        <StarIcon className="mr-2" />
+                        Favorite
+                    </Toggle>
                 </div>
             </main>
         </div>
