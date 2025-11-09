e0069b4e5541057aa1a9c46d86f5da86e19edbaf
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-08 21:14:12 -0500

    Redid the favorites page slightly

--- frontend/app/favorites/page.tsx
+++ frontend/app/favorites/page.tsx
@@ -1,15 +1,18 @@
 import FavoriteMoviesList from "@/components/ui/favorite-movies-list";
 import { Navbar } from "@/components/ui/navbar";
+import { Card } from "@/components/ui/card";
 
 export default function FavoriteMoviesListPage() {
     return (
         <div className="flex min-h-screen">
             <main className="flex flex-col min-h-screen w-full">
                 <Navbar />
-                <div className="m-auto">
-                    <h1 className="lg:text-4xl md:text-3xl sm:text-2xl">Your Favorites:</h1>
-                    <FavoriteMoviesList />
-                </div>
+                <div className="m-auto flex flex-col items-center">
+                    <h1 className="lg:text-4xl md:text-3xl sm:text-2xl mb-10">Your Favorites:</h1>
+                    <div className="m-auto min-w-[1152px]">
+                        <FavoriteMoviesList />
+                    </div>
+                </div>                
             </main>
             
         </div>
